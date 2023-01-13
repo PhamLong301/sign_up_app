324f85cb8f6671cecee171e3609a21792aeb2fcd
phamlong312 <longde345@gmail.com>
2023-01-13 08:45:02 +0700

    Update dialpad_widget.dart

--- lib/widgets/dialpad_widget.dart
+++ lib/widgets/dialpad_widget.dart
@@ -11,7 +11,7 @@ class DialPadWidget extends StatefulWidget {
   final SIPUAHelper? _helper;
   const DialPadWidget(this._helper, {Key? key}) : super(key: key);
   @override
-  // ignore: library_private_types_in_public_api
+  // ignore:   
   _MyDialPadWidget createState() => _MyDialPadWidget();
 }
 
