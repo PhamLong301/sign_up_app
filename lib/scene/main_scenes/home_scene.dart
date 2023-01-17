c59edea37b25d8a4649e0152954c0970f587bcbd
phamlong312 <longde345@gmail.com>
2023-01-17 14:43:36 +0700

    Update home_scene.dart

--- lib/scene/main_scenes/home_scene.dart
+++ lib/scene/main_scenes/home_scene.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:sign_up_app/scene/contact/dialpad_scene.dart';
 import 'package:sign_up_app/scene/contact/near_contacts_scene.dart';
