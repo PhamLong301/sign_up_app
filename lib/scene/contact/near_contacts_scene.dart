adfdac4f2527afe16d8786f1b6284415e7a7494a
phamlong312 <longde345@gmail.com>
2023-01-18 09:05:33 +0700

    Update near_contacts_scene.dart

--- lib/scene/contact/near_contacts_scene.dart
+++ lib/scene/contact/near_contacts_scene.dart
@@ -27,35 +27,44 @@ class _NearContactSceneState extends State<_NearContactScene> {
       body: Column(
         children: [
           Container(
-            height: 300,
+            height: 250,
             alignment: Alignment.center,
-            child: const Text('Contacts',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30),
+            child: const Text(
+              'Contacts',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
             ),
           ),
           SizedBox(
             height: 40,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.end,
-              children:const [
-                IconButton(onPressed: null,
-                    icon: Icon(Icons.category_rounded,
-                    color: Colors.black,),
+              children: const [
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.category_rounded,
+                    color: Colors.black,
                   ),
-                IconButton(onPressed: null,
-                  icon: Icon(Icons.search,
-                    color: Colors.black,),
                 ),
-                IconButton(onPressed: null,
-                  icon: Icon(Icons.more_vert,
-                    color: Colors.black,),
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                ),
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.more_vert,
+                    color: Colors.black,
+                  ),
                 ),
               ],
             ),
           ),
-          Expanded(child: ListView(
+          Expanded(
+              child: ListView(
             children: const [
               NearContactsWidget(),
             ],
@@ -65,4 +74,3 @@ class _NearContactSceneState extends State<_NearContactScene> {
     );
   }
 }
-
