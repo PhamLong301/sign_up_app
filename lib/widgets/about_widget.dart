371c0cd3e35fa6c76dab99a9efe231e8cca77feb
phamlong312 <longde345@gmail.com>
2023-01-31 15:47:00 +0700

    Update about_widget.dart

--- lib/widgets/about_widget.dart
+++ lib/widgets/about_widget.dart
@@ -23,10 +23,7 @@ class AboutWidget extends StatelessWidget {
                     style: TextStyle(
                       fontSize: 25.0,
                     ),
-                    colors: [
-                      Colors.blue,
-                      Colors.teal,
-                    ],
+                    colors: [Colors.red, Colors.orange, Colors.yellow],
                   ),
                 ],
               ),
